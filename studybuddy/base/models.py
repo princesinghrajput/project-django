453182e0f714dfc0c88b4ef1c32b5c9325f04a15
princesinghrajput <psr8084@gmail.com>
2024-07-08 09:50:40 +0530

    updated

--- studybuddy/base/models.py
+++ studybuddy/base/models.py
@@ -1,4 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    #host
+    host=models.ForeignKey(User, on_delete=models.SET_NULL, null=True )
+    name = models.CharField(max_length=100)
+
+    #topic
+    topic=models.ForeignKey(Topic, on_delete=models.CASCADE, null=True)
+ 
+    description = models.TextField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    user= models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, null=True)
+    body=models.TextField()
+
+
+
+    def __str__(self):
+        return self.body[:50]
+
 
